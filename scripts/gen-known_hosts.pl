68c859d08ae8644901ef6c41559d72819e429ae4
root <root@lionssrv1.epfl.ch>
2015-05-10 10:37:35 +0200

    Make gen-known_hosts.pl more robust
    * Truncate error log
    * Always log the timestamp
    * Add -o option, so that the file won't get created in case of failure
    * Check return of writes and closes

--- scripts/gen-known_hosts.pl
+++ scripts/gen-known_hosts.pl
@@ -8,7 +8,7 @@ gen-known_hosts.pl - Create the ssh known_hosts file from fact data in Foreman
 
 =head1 SYNOPSIS
 
-  gen-known_hosts.pl
+  gen-known_hosts.pl [ -o <outputfile> ]
 
 =head1 DESCRIPTION
 
@@ -18,8 +18,13 @@ format.
 
 =cut
 
+use Getopt::Long;
+
 # Attempt to save stderr somewhere - No biggie if that fails
-open(STDERR, ">> /var/log/puppet/gen-known_hosts.log");
+open(STDERR, "> /var/log/puppet/gen-known_hosts.log");
+
+my $now = localtime(time);
+warn "[$now] Running $0 " . join(" ", @ARGV);
 
 if (! defined $ENV{HOME}) {
   $ENV{HOME} = "/root";
@@ -31,9 +36,28 @@ do {
   defined(<U_CAN_TOUCH_THIS>);  # Skip header
 } or die "Stop! No hammertime: $!";
 
+# Redirect only now, so that the file doesn't get created in case of failure
+our $outputfile;
+GetOptions("o=s" => sub {
+  (undef, $outputfile) = @_;
+  warn "Redirecting to $outputfile";
+  open(STDOUT, ">", $outputfile) or
+    die "Cannot open $outputfile for writing: $!";
+});
+sub fatal_cannot_write {
+  if ($outputfile) {
+    die "Cannot write to $outputfile: $!";
+    unlink($outputfile);
+  } else {
+    die "Cannot write to standard output: $!";
+  }
+}
+
 while(<U_CAN_TOUCH_THIS>) {
   chomp;
   my ($fqdn, undef, $pubkey) = split m/,/;
-  print "$fqdn ssh-rsa $pubkey\n";
+  print "$fqdn ssh-rsa $pubkey\n" or fatal_cannot_write;
 }
 
+close(STDOUT) or fatal_cannot_write;
+exit 0;
